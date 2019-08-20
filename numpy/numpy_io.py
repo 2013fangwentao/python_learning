94571777cc680f650710c55c56655c5aa04bf6c9
fwt-whu-ubuntu <1280269817@qq.com>
2019-08-20 11:29:00 +0800

    numpy画图和文件读取

---
+++ numpy/numpy_io.py
@@ -0,0 +1,46 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+
+
+def main():
+    print("length of argv is %d" % len(sys.argv))
+    if (len(sys.argv) < 2):
+        print("参数不足")
+        return
+    file_path = sys.argv[1]  # 结果文件
+    pos_data = np.loadtxt(file_path)
+    time = pos_data[..., 1]
+    xyz = pos_data[..., 2:5]
+    vxyz = pos_data[..., 5:8]
+    att = pos_data[..., 8:11]
+    xyz_mean = np.mean(xyz, axis=0)
+    xyz = xyz - xyz_mean
+    plt.figure(1)
+    plt.title("xyz")
+    plt.plot(time, xyz)
+    plt.ylabel("xyz(m)")
+    plt.xlabel("time(s)")
+    plt.grid()
+    # plt.show()
+
+    plt.figure(2)
+    plt.title("vxyz")
+    plt.plot(time, vxyz)
+    plt.ylabel("vxyz(m)")
+    plt.xlabel("time(s)")
+    plt.grid()
+    # plt.show()
+
+    plt.figure(3)
+    plt.title("att")
+    plt.plot(time, att)
+    plt.ylabel("att(deg)")
+    plt.xlabel("time(s)")
+    plt.grid()
+    plt.show()
+
+
+
+if __name__ == "__main__":
+    main()
